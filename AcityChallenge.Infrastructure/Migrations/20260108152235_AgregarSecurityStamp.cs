d05e11a19c7e651d7908b3bfa29ca92c5bdb9b82
Omar Vásquez <omarvs91@gmail.com>
2026-01-08 10:45:32 -0500

    Migración de base de datos para soporte de SecurityStamp.

---
+++ AcityChallenge.Infrastructure/Migrations/20260108152235_AgregarSecurityStamp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcityChallenge.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarSecurityStamp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SecurityStamp",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SecurityStamp",
+                table: "Usuarios");
+        }
+    }
+}
