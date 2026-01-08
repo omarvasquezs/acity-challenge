ff058b8c4f69dde2ca3cc141b037b35d27c523e1
Omar Vásquez <omarvs91@gmail.com>
2026-01-08 02:02:58 -0500

    Registro del contexto de base de datos en la capa API y configuración de la cadena de conexión.

--- AcityChallenge.Api/Program.cs
+++ AcityChallenge.Api/Program.cs
@@ -1,4 +1,10 @@
+using AcityChallenge.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -21,7 +27,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
